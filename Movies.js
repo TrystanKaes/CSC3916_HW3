20afe4d5a1571c664cc8e430d7b4fef5a83c8d90
Trystan Kaes <baby.hughee@gmail.com>
2020-03-10 15:44:31 -0600

    Corrected the mongoose scheme to have genre as an enum.

--- Movies.js
+++ Movies.js
@@ -8,14 +8,14 @@ mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DB, { useNewUrlParser: true } );
 mongoose.set('useCreateIndex', true);
 
-const Genre = ["Action", "Adventure", "Comedy", "Drama", "Fantasy", "Horror", "Mystery", "Thriller", "Western"];
-
 // Movie schema
 var MovieSchema = new Schema({
     title: String,
     yearReleased: Date,
-    target: {
-        genre: {type: String, enum: Genre}
+    genre: {
+        type: String,
+        enum: ["Action", "Adventure", "Comedy", "Drama", "Fantasy", "Horror", "Mystery", "Thriller", "Western","unlisted"]
+        default: "unlisted"
     },
     actors: [String]
 });
